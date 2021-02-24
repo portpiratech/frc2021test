bd80d0771423654a8d78ab09e5192ae46a614775
Portpiratech <portpiratech@gmail.com>
2021-02-23 19:45:20 -0600

    Updated

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.TestCommand;
+import frc.robot.subsystems.TestSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,14 +19,31 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
+  public static TestSubsystem testSubsystem = new TestSubsystem();
+  
+  public static XboxController driverController;
+  public static XboxController operatorController;
+  public static JoystickButton backButtonDriver;
+  public static JoystickButton yButtonDriver;
+  public static JoystickButton rightBumperDriver;
+  public static JoystickButton yButtonOperator;
+  public static JoystickButton xButtonOperator;
+  public static JoystickButton aButtonOperator;
+  public static JoystickButton bButtonOperator;
+  public static JoystickButton backButtonOperator;
+  public static JoystickButton startButtonOperator;
+  public static JoystickButton leftBumperOperator;
+  public static JoystickButton rightBumperOperator;
+  public static JoystickButton aButtonDriver;
+  public static JoystickButton bButtonDriver;
+  public static JoystickButton xButtonDriver;
+  public static JoystickButton startButtonDriver;
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+
+    aButtonDriver.whenPressed(new TestCommand());
   }
 
   /**
@@ -34,7 +52,27 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    driverController = new XboxController(Constants.driverControllerId);
+    operatorController = new XboxController(Constants.operatorControllerId);
+
+    // name buttons to include which controller
+    backButtonDriver = new JoystickButton(driverController, 7);
+    yButtonDriver = new JoystickButton(driverController, 4);
+    rightBumperDriver = new JoystickButton(driverController, 6);
+    yButtonOperator = new JoystickButton(operatorController, 4);
+    xButtonOperator = new JoystickButton(operatorController, 3);
+    aButtonOperator = new JoystickButton(operatorController, 1);
+    bButtonOperator = new JoystickButton(operatorController, 2);
+    backButtonOperator = new JoystickButton(operatorController, 7);
+    startButtonOperator = new JoystickButton(operatorController, 8);
+    leftBumperOperator = new JoystickButton(operatorController, 5);
+    rightBumperOperator = new JoystickButton(operatorController, 6);
+    aButtonDriver = new JoystickButton(driverController, 1);
+    bButtonDriver = new JoystickButton(driverController, 2);
+    xButtonDriver = new JoystickButton(driverController, 3);
+    startButtonDriver = new JoystickButton(driverController, 8);
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -43,6 +81,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
